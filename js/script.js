43f32ca19f190f7f934337d0dfc1d9f4a8aacce7
GCpeeper <gpeeper@eastsideprep.org>
2024-11-13 11:09:14 -0800

    Games Pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 
+
 function subscribe() {
-    
+    email = document.getElementById("email").value;
+    if (email == "") {
+        alert("Please enter a valid email address.");
+        return;
+    } else if (!email.includes("@")) {
+        alert("Please enter a valid email address.");
+        return;
+    } else {
+        alert("Thank you for subscribing!");    
+    }
 }
