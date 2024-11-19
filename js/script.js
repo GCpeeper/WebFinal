1ffeb669229193ae717699ce51216a38d07ce581
GCpeeper <gcpeeper@hotmail.com>
2024-11-18 18:06:06 -0800

    finished contact page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var subscribed = false;
+var message;
+var email;
+var name;
 
 function subscribe() {
     email = document.getElementById("email").value;
@@ -21,4 +24,53 @@ function unlock() {
     } else {
         content_lock.innerHTML = "Please subscribe to view this content.";
     }
+}
+
+function send() {
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    name = document.getElementById("name").value;
+    var alertMessage  = "";
+    if (name == "" || email == "" || message == "") {
+        alertMessage  += ("Please fill out all fields.\n");
+    }
+    if (email == "" || !email.includes("@")) {
+        alertMessage  += ("Please enter a valid email address.");
+    }
+
+    if (alertMessage  != "") {
+        alert(alertMessage);
+        return;
+    } else {
+        alertMessage  += ("Thank you for your message! We will get back to you shortly.");
+    }
+    alert(alertMessage);
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}
+
+function expandForm() {
+    var form = document.getElementById("message");
+    var message = document.getElementById("message").value;
+    var enters = message.split("\n").length;
+    console.log(enters);
+    var lines = message.length/55;
+    lines = Math.floor(lines);
+    console.log(lines);
+    var total = lines + enters;
+
+    if (total < 3) {
+        form.rows = 3;
+        return 3;
+    } else {
+        form.rows = total+1;
+        return total+1;
+    }
+
+}
+
+function generateEmail() {
+
 }
