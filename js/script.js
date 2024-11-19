856bcdb420782459d0fc21624697e8526b84731a
GCpeeper <gpeeper@eastsideprep.org>
2024-11-19 10:35:43 -0800

    Added the content lock

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-var subscribed = false;
 var message;
 var email;
 var name;
@@ -13,18 +12,35 @@ function subscribe() {
         return;
     } else {
         alert("Thank you for subscribing!");    
-        subscribed = true;
+        document.cookie = "subscribed=true";
     }
 }
 
 function unlock() {
     var content_lock = document.getElementById("content_lock");
+    var subscribed = getCookie("subscribed");
     if (subscribed) {
         content_lock.innerHTML = "Enjoyed this post? Subscribe for more!";
     } else {
         content_lock.innerHTML = "Please subscribe to view this content.";
     }
 }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return;
+  }
 
 function send() {
     email = document.getElementById("email").value;
